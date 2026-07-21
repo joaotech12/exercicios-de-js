590e61531f5d5f821d264b4a28fe58036a25aa9d
joaotech12 <zeroo01010010@gmail.com>
2026-07-20 21:49:02 -0300

    ok

---
+++ script2.js
@@ -0,0 +1,7 @@
+const precoproduto = prompt("Digite o preço:")
+const percentual = prompt("Digite o valor do desconto (%):");
+
+const valorDesconto = (precoproduto * percentual) / 100;
+alert("O valor do desconto é R$ " + valorDesconto)
+
+//calculo de IMC 
