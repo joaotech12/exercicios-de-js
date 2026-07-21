8ae9f003500a5e891aeb7b6f438328e5591d8d27
joaotech12 <zeroo01010010@gmail.com>
2026-07-20 21:49:50 -0300

    Add files via upload

---
+++ execicios js/script.js
@@ -0,0 +1,34 @@
+const nome = "João" 
+const sobrenome = "Tech" 
+const nomeCompleto = nome +  " " +sobrenome
+const templateString =  `Olá, meu nome completo é: ${nome} ${sobrenome} `
+ 
+
+console.log(nomeCompleto)
+console.log(templateString)
+
+
+//idade 
+const idade = 20
+const ano = 2005
+const templateString2 = `Nasci em ${ano} e tenho ${idade} anos`
+ console.log(templateString2)
+
+ //numero 3 
+if(idade%4==0){
+    console.log ("O número proposto é par")
+}else{
+    console.log ("O número proposto é impar")
+
+}
+
+const valor = 4000
+
+const valordesconto = valor * 0.10 
+const valordescontoaserdescontado = valor - valordesconto 
+console.log(valordesconto)
+
+
+
+
+
