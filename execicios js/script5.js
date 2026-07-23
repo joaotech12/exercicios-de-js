01b0fd2123a17e5ea0efb82239fb7feda802fc29
joaotech12 <zeroo01010010@gmail.com>
2026-07-22 21:37:24 -0300

    ok

---
+++ execicios js/script5.js
@@ -0,0 +1,17 @@
+//lista
+
+//const numero =[1,2,3,4,5];
+//const resultado = [];
+//for (let i = 0; i < numero.length; i++) {
+//let conta = numero[i] * i;
+//resultado.push(conta);
+
+//}
+//console.log(resultado);
+
+const nomes =["felipe","edu","gabriel"];
+const nomesInvertidos = [];
+for (let i = nomes.length - 1; i >= 0; i--){
+    nomesInvertidos.push(nomes[i]);
+}
+console.log(nomesInvertidos)
